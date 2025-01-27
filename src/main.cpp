446ce7fa2d1f0b0e825ab64e2f39fb74cc55b5e8
cinnamon byte <cinnamonbyte@gmail.com>
2025-01-27 05:44:42 -0600

    Documentation, rename/add shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
+#include <GL/glew.h>	//header file for sonething
 #include <cstdio>
 #include <stdio.h>
 #include <stdlib.h>
-#include <GL/glew.h>	//header file for sonething
 #include <GLFW/glfw3.h>	//window, keyboard and mouse inputs
 #include <glm/glm.hpp>
 #include <filesystem>
-#include<iostream>
+#include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL 
+#include <glm/gtx/transform.hpp>
+
 using namespace glm;
 #include <common/shader.hpp>
 GLFWwindow* window;
@@ -24,13 +28,14 @@ int main(void){
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(1024,768, "tutorial 01", NULL, NULL);
+	window = glfwCreateWindow(400,400, "RaySpy", NULL, NULL);
 	glfwMakeContextCurrent(window);	//assign context to current windows so other functions can work on it, you can switch context to focus on different windows
 	if (window == NULL){
 		fprintf(stderr, "Failed to open GLFW window.");
 		glfwTerminate();
 		return -1;
 	}
+	/*glfwSetWindowSizeLimits(window, 1024, 768, 1024, 768);*/
 	if(glewInit()!= GLEW_OK){
 		fprintf(stderr, "failed to init GLEW\n");
 	}
@@ -43,7 +48,7 @@ int main(void){
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
-	GLuint programID = LoadShaders("../src/SimpleVertexShader.txt", "../src/SimpleFragmentShader.txt");
+	GLuint programID = LoadShaders("../shaders/SimpleVertexShader.txt", "../shaders/SimpleFragmentShader.txt");
 
 
 	static const GLfloat g_vertex_buffer_data[]={
@@ -56,12 +61,18 @@ int main(void){
 	glGenBuffers(1,&vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER,vertexbuffer); 
 	glBufferData(GL_ARRAY_BUFFER,sizeof(g_vertex_buffer_data),g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	mat4 MyMatrix;
+	vec4 MyVector;
+	vec4 transformed = MyMatrix * MyVector;
 	do{
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(programID);
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER,vertexbuffer);
-		glVertexAttribPointer(0, 3,GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glVertexAttribPointer(0, 3,GL_FLOAT, GL_FALSE, sizeof(GLfloat)*3, (void*)0);
+		//describes how the array is structured
+		//position in array, no of components in aray (x,y,z), data type, if we want to normalize data, offset till next item in memory, offset location of item within the array
 		glDrawArrays(GL_TRIANGLES, 0,3);
 		glDisableVertexAttribArray(0);
 		glfwSwapBuffers(window);
